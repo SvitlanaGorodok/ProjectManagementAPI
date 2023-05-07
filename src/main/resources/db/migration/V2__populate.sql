3edad6c5bc2e56f297700df6fffa71fe4149cf98
svetl <svetlana.akulowa@gmail.com>
2023-05-07 21:21:48 +0300

    add populated data

--- src/main/resources/db/migration/V2__populate.sql
+++ src/main/resources/db/migration/V2__populate.sql
@@ -11,10 +11,20 @@ INSERT INTO positions (id, "name") VALUES ('e814e6c8-64f4-4c3a-97be-cde232852686
 
 INSERT INTO employees (id, first_name, last_name, email, position_id, level_id)
 VALUES ('a8dd994d-c7c4-4bfb-8ed8-3e1a7a691ee1', 'Taras', 'Shevchenko', 'tshevchenko@gmail.com',
-		'ead31a24-efb5-4ceb-a3be-89e2c8ac9d02', '4e3c27be-76de-496a-bed2-fb2dcb71ab7a');
+		'ead31a24-efb5-4ceb-a3be-89e2c8ac9d02', 'e9072bc9-9402-45eb-8e9a-cc088692879c'),
+        ('5c3814d7-2d48-4ac3-92de-7a6fbe819240', 'Lesya', 'Ukrainka', 'lukrainka@gmail.com',
+        '4e720329-ce31-49bb-b713-0da9e6becab0', 'e9072bc9-9402-45eb-8e9a-cc088692879c'),
+        ('754f30cb-58cf-4dbb-be0d-c0d57a2bbe84', 'Ivan', 'Franko', 'ifranko@gmail.com',
+        'e814e6c8-64f4-4c3a-97be-cde232852686', 'e9072bc9-9402-45eb-8e9a-cc088692879c');
 
 INSERT INTO projects (id, "name", start_date, end_date)
-VALUES ('72cf247a-ed44-4c92-bcaa-457bb4d7c011', 'Kobzar', '2023-01-01', '2023-01-31');
+VALUES ('72cf247a-ed44-4c92-bcaa-457bb4d7c011', 'Kobzar', '2021-01-01', '2022-01-31'),
+        ('1065b4ae-d613-476b-a1b6-0eb5dc7a798a', 'Lisova Pisnia', '2020-01-01', '2021-01-31'),
+        ('8ca38881-ca4e-4536-8a57-65cba5ef675b', 'Ukraina', '2022-01-01', '2023-01-31');
 
 INSERT INTO projects_employees_relation (project_id, employee_id)
-VALUES ('72cf247a-ed44-4c92-bcaa-457bb4d7c011','a8dd994d-c7c4-4bfb-8ed8-3e1a7a691ee1');
+VALUES ('72cf247a-ed44-4c92-bcaa-457bb4d7c011','a8dd994d-c7c4-4bfb-8ed8-3e1a7a691ee1'),
+        ('1065b4ae-d613-476b-a1b6-0eb5dc7a798a','5c3814d7-2d48-4ac3-92de-7a6fbe819240'),
+        ('8ca38881-ca4e-4536-8a57-65cba5ef675b','a8dd994d-c7c4-4bfb-8ed8-3e1a7a691ee1'),
+        ('8ca38881-ca4e-4536-8a57-65cba5ef675b','5c3814d7-2d48-4ac3-92de-7a6fbe819240'),
+        ('8ca38881-ca4e-4536-8a57-65cba5ef675b','754f30cb-58cf-4dbb-be0d-c0d57a2bbe84');
